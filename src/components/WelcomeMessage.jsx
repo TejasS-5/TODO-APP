5d693fed81433e0f1aab8b2fc9657ef9ae3a39c0
Tejas Sontakke <sontakketejas5@gmail.com>
2026-08-30 16:49:35 +0530

    Update WelcomeMessage.jsx

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -3,7 +3,7 @@ import styles from "./WelcomeMessage.module.css";
 const WelcomeMessage = (todoItems) => {
   return (
     todoItems.length === 0 && (
-      <p className={styles.welcome}>ENJOY YOUR PRECIOUS DAY HERE</p>
+      <p className={styles.welcome}>ENJOY YOUR PRECIOUS DAY</p>
     )
   );
 };
